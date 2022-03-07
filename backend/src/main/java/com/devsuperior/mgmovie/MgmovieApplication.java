df308fd4ef42000aab9b6b0214e64d412a6f615f
marepifanio <marcelog.epifanio@gmail.com>
2022-03-07 14:48:25 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/mgmovie/MgmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.mgmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/mgmovie/MgmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.mgmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
